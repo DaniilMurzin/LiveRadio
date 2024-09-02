2a9f56006b2f45a11c2a866a4b778bd4f2744d30
Daniil Murzin <danmur92@mail.ru>
2024-09-02 20:24:06 +0500

    Initial Commit

---
+++ LiveRadioApp/LiveRadioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveRadioAppApp.swift
+//  LiveRadioApp
+//
+//  Created by Daniil Murzin on 02.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LiveRadioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

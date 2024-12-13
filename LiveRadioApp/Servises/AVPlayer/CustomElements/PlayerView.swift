7cecf1c89ff5254ce5740c2d8490c00a43ceef89
Daniil Murzin <danmur92@mail.ru>
2024-12-13 15:24:08 +0500

    add RadioPlayer logic

---
+++ LiveRadioApp/Servises/AVPlayer/CustomElements/PlayerView.swift
@@ -0,0 +1,37 @@
+//
+//  PlayerView.swift
+//  LiveRadioApp
+//
+//  Created by Daniil Murzin on 18.09.2024.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    typealias Action = () -> Void
+    
+    let backwardButtonAction: Action
+    let forwardButtonAction: Action
+    let playButtonAction: Action
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            TrackButtonsView(
+                action: backwardButtonAction,
+                direction: .backward)
+              
+            PlayButtonView(action: playButtonAction)
+            
+            TrackButtonsView(
+                action: forwardButtonAction,
+                direction: .forward)
+        }
+    }
+}
+
+#Preview {
+        PlayerView(
+            backwardButtonAction: {},
+            forwardButtonAction: {},
+            playButtonAction: {})
+}
